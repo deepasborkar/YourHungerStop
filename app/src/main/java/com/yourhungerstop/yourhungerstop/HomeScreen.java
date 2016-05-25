9606ae0dc71ee9e4d7b2117cd47808f54f2785ff
deepasborkar <deepagude@gmail.com>
2016-05-25 10:53:18 +0300

    Added code to retrieve data from URL and create JSON Object

--- app/src/main/java/com/yourhungerstop/yourhungerstop/HomeScreen.java
+++ app/src/main/java/com/yourhungerstop/yourhungerstop/HomeScreen.java
@@ -1,13 +1,60 @@
 package com.yourhungerstop.yourhungerstop;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONObject;
 
 public class HomeScreen extends AppCompatActivity {
+
+    private final static String GET_RECENT_POSTS="http://www.yourhungerstop.com/api/get_recent_posts/";
+    private Button btnSubmit;
+    private TextView tvWordpressData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new FetchDataTask().execute(GET_RECENT_POSTS);
+            }
+        });
+
+
     }
-}
+    private class FetchDataTask extends AsyncTask<String, Void, JSONObject> {
+
+
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+
+            JSONParser jParser = new JSONParser();
+
+            // Getting JSON from URL
+            JSONObject json = jParser.getRecentPostsFromUrl(params[0]);
+            return json;
+
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject dataFetched) {
+            //parse the JSON data and then display
+            //parseJSON(dataFetched);
+        }
+
+    }
+    }
+
+
+
+
+
---
+++ app/src/main/java/com/yourhungerstop/yourhungerstop/JSONParser.java
@@ -0,0 +1,74 @@
+package com.yourhungerstop.yourhungerstop;
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+
+/**
+ * Created by Deepa on 25-05-2016.
+ */
+public class JSONParser {
+
+
+    /**
+     * This method connects to the URL provided as input parameter to it and reads the data from the
+     * URL. This data is further converted to a JSONObject and returned
+     *
+     * @param targetUrl : URL from where data needs to be retrieved
+     * @return JSONObject : containing data from the URL
+     */
+    public JSONObject getJSONFromUrl(String targetUrl) {
+        JSONObject jObject = null;
+        HttpURLConnection urlConnection;
+        StringBuilder result = new StringBuilder();
+        String json = "";
+
+        try {
+            // Create URLConnection object with the url passed to the method by the main UI thread
+            URL url = new URL(targetUrl);
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            //read the data from the URL
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+
+            json = result.toString();
+        } catch (MalformedURLException e) {
+            Log.e("URL data error", "JSONParser.getJSONFromUrl: URL is invalid" + e.toString());
+        } catch (SocketTimeoutException e) {
+            Log.e("URL data error", "JSONParser.getJSONFromUrl: Data retrieval or connection timed out" + e.toString());
+        } catch (IOException e) {
+            Log.e("URL data error", "JSONParser.getJSONFromUrl: Could not read response body" + e.toString());
+        } catch (Exception e) {
+            Log.e("URL data error", "JSONParser.getJSONFromUrl" + e.toString());
+        }
+
+        //create json object from the data received from the input stream
+        try {
+            jObject = new JSONObject(json);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "JSONParser.getJSONFromUrl: Error parsing data " + e.toString());
+        }
+        return jObject;
+    }
+
+    public JSONObject getRecentPostsFromUrl(String url) {
+           return getJSONFromUrl(url);
+    }
+}
